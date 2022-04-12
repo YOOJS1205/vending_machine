13b304fe2c80802b05a126b908076f42803c4efc
YOOJS1205 <junsang8921@gmail.com>
2022-04-12 23:06:40 +0900

    0412

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const FirstLists = document.querySelector('.lists:first-child');
-const SecondLists = document.querySelector('.lists:last-child');
+const Machine = document.querySelector('.machine');
 const List = document.querySelectorAll('.list');
 const GetList = document.querySelector('.get-list');
+const Get = document.querySelector('.get');
+const BeverageList = document.querySelector('.beverage-list');
 
 // 상품 담기 => 이벤트 위임 사용
-FirstLists.addEventListener('click', (e) => {
-    for (let i = 0; i < 3; i++) {
+Machine.addEventListener('click', (e) => {
+    for (let i = 0; i < 6; i++) {
         if (e.target === List[i]) {
             const Detail = `<section class="detail">
                             <img src="${e.target.childNodes[1].src}" alt="product-info">
                             <p>${e.target.childNodes[3].innerHTML}</p>
                         <section>`;
             GetList.insertAdjacentHTML('beforeend', Detail);
+            Get.addEventListener('click', () => {
+                BeverageList.insertAdjacentHTML('beforeend', Detail);
+            })
         }
     }
 })
