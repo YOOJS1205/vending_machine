953a932cd82173abc498eafb22d0e2c1e5ff8d7d
YOOJS1205 <junsang8921@gmail.com>
2022-04-16 18:55:43 +0900

    0416

--- app.js
+++ app.js
@@ -11,24 +11,22 @@ Machine.addEventListener('click', (e) => {
     for (let i = 0; i < 6; i++) {
         if (e.target === List[i]) {
             clickNumArray[i]++;
-            console.log(clickNumArray);
-            const Detail = `<section class="detail">
+            if (clickNumArray[i] == 1) {
+                var Detail = `<section class="detail">
                             <img class="beverage-pic" src="${e.target.childNodes[1].src}" alt="product-info">
                             <p class="beverage-name">${e.target.childNodes[3].innerHTML}</p>
-                            <p class="beverage-num">${clickNumArray[i]}</p>
+                            <p class="beverage-num ${e.target.childNodes[3].innerHTML}">${clickNumArray[i]}</p>
                         <section>`;
-            // if (clickNumArray[i] === 1) {
-            //     GetList.insertAdjacentHTML('beforeend', Detail);
-            // }
-            // else {
-            //     const parser = new DOMParser();
-            //     console.log(parser.parseFromString(Detail, 'text/xml'));
-            // }
-            GetList.insertAdjacentHTML('beforeend', Detail);
-            Get.addEventListener('click', () => {
-                BeverageList.insertAdjacentHTML('beforeend', Detail);
-            })
+                GetList.insertAdjacentHTML('beforeend', Detail);
+            }
+            else {
+                document.querySelector(`.${e.target.childNodes[3].innerHTML}`).innerHTML = clickNumArray[i];
+            }
         }
 
     }
+})
+
+Get.addEventListener('click', () => {
+    BeverageList.insertAdjacentHTML('beforeend', GetList.outerHTML);
 })
