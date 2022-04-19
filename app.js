9f94144705067f627b06cd74f76f6e856f4c19e3
YOOJS1205 <junsang8921@gmail.com>
2022-04-19 17:40:23 +0900

    flex to grid

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const Machine = document.querySelector('.machine');
 const List = document.querySelectorAll('.list');
-const GetList = document.querySelector('.get-list');
-const Get = document.querySelector('.get');
+const CartList = document.querySelector('.cart-list');
+const GetItems = document.querySelector('.get-items');
 const BeverageList = document.querySelector('.beverage-list');
 
 var clickNumArray = [0, 0, 0, 0, 0, 0];
+
+// 장바구니 리스트 초기화 함수....?
+const removeAllChild = () => {
+    while (CartList.hasChildNodes()) {
+        CartList.removeChild(CartList.firstChild);
+    }
+}
 
 // 상품 담기 => 이벤트 위임 사용
 Machine.addEventListener('click', (e) => {
@@ -17,18 +24,17 @@ Machine.addEventListener('click', (e) => {
                             <p class="beverage-name">${e.target.childNodes[3].innerHTML}</p>
                             <p class="beverage-num ${e.target.childNodes[3].innerHTML}">${clickNumArray[i]}</p>
                         <section>`;
-                GetList.insertAdjacentHTML('beforeend', Detail);
+                CartList.insertAdjacentHTML('beforeend', Detail);
             }
             else {
                 document.querySelector(`.${e.target.childNodes[3].innerHTML}`).innerHTML = clickNumArray[i];
             }
         }
-
     }
 })
 
-Get.addEventListener('click', () => {
-    for (let i = 1; i < GetList.childNodes.length; i++) {
-        BeverageList.insertAdjacentHTML('beforeend', GetList.childNodes[i].outerHTML);
+GetItems.addEventListener('click', () => {
+    for (let i = 0; i < CartList.childNodes.length; i++) {
+        BeverageList.insertAdjacentHTML('beforeend', CartList.childNodes[i].outerHTML);
     }
 })
