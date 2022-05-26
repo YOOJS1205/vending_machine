ac43259710308b54b8dfba25fbe338f197e8debc
YOOJS1205 <junsang8921@gmail.com>
2022-05-27 00:38:25 +0900

    0526 입금 버튼 기능 구현, 획득 버튼 추가 기능 구현

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // 상품 정보 데이터 불러오기
 import beverageList from './beverage.js';
-console.log(beverageList);
 
 const lists = document.querySelector('.lists');
 const cartList = document.querySelector('.cart-list');
@@ -8,7 +7,7 @@ const getButton = document.querySelector('.get-items');
 const getList = document.querySelector('.beverage-list');
 
 // 음료수 별 클릭 횟수 배열로 담기
-const clickNum = Array(beverageList.length).fill(0);
+let clickNum = Array(beverageList.length).fill(0);
 
 // 상품 갯수만큼 상품 버튼 생성
 beverageList.forEach(item => {
@@ -35,7 +34,7 @@ const buttons = [...document.querySelectorAll('.list')];
 
 // 음료 클릭 시 장바구니에 담기는 기능 구현
 lists.addEventListener('click', e => {
-    if (e.target.closest('.list').nodeName === 'BUTTON' && clickNum[buttons.indexOf(e.target.closest('.list'))] == 0) {
+    if (e.target.closest('.list').nodeName === 'BUTTON') {
         const cartItem = document.createElement('section');
         cartItem.className = 'detail';
 
@@ -58,10 +57,37 @@ lists.addEventListener('click', e => {
     }
 });
 
-// 획득 버튼 클릭 시 획득한 음료 칸에 담기는 기능 구현
+// 획득 버튼
+// 기능 1. 획득한 음료 칸으로 음료들 이동
+// 기능 2. 음료 클릭된 수 배열 초기화
+// 기능 3. 획득한 음료 총 금액 반영
+
+const cost = document.querySelector('.cost');
+
+let overall = 0;
 getButton.addEventListener('click', () => {
-    // forEach nodelist vs array
     [...cartList.childNodes].forEach(item => {
         getList.appendChild(item);
     });
+
+    for (let i = 0; i < clickNum.length; i++) {
+        overall += clickNum[i];
+    }
+
+    cost.innerText = `총금액: ${overall * 1000}원`;
+
+    clickNum = Array(beverageList.length).fill(0);
+})
+
+// 입금 버튼 클릭 시 입금액 만큼 소지금이 증가하는 기능 구현
+const deposit = document.querySelector('.deposit');
+const depositButton = document.querySelector('.get-deposit');
+const myMoney = document.querySelector('.my-money');
+
+let ownMoney = 0;
+
+depositButton.addEventListener('click', () => {
+    ownMoney += parseInt(deposit.value);
+    myMoney.innerText = `${ownMoney}원`;
+    deposit.value = '';
 })
